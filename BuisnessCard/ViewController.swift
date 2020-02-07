19ba6129b64234278ac0acd67ff9af1d43ac336c
fredericfilsoof <filsoof95@hotmail.com>
2020-02-07 16:28:34 +0100

    Initial Commit

---
+++ BuisnessCard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BuisnessCard
+//
+//  Created by Frederic Filsoof on 07/02/2020.
+//  Copyright © 2020 Frederic Filsoof. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
